3edfbb1b9a4f6aa09fab6503592daca59cdd7909
MMYaseen7 <yaseenmohommad7@gmail.com>
2023-10-30 13:02:55 +0530

    searchPaper implementation

--- server/src/controllers/paperController.js
+++ server/src/controllers/paperController.js
@@ -1,5 +1,10 @@
+// searchPaper string(searchterm) matching title in dbs
+import PaperSchema from "../models/PaperSchema"
 const searchPaper = async (req, res) => {
-
+    
+    let search = req.query.keyword;
+    let queryval = {title: req.query.keyword};
+    PaperSchema.
 };
 
 const recommendPaper = () => {
