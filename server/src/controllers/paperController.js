d7ae82949e8a165400c77305d797c5d12aa9dd63
kshitij-n24 <141441196+kshitij-n24@users.noreply.github.com>
2023-10-30 13:09:56 +0530

    Add paperController to src/controllers

--- server/src/controllers/paperController.js
+++ server/src/controllers/paperController.js
@@ -12,7 +12,9 @@ const recommendPaper = () => {
 
 // Send the paper matched according to the title sent as query param 
 const sendPaper = (req, res) => {
+  const paperTitle = req.query.title;
+
   
 };
 
-export {searchPaper, addComment};
+export {searchPaper, addComment, recommendPaper, sendPaper};
