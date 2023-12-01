396b11ffff821b672a1e9e1f104e33a31466843c
kshitij-n24 <141441196+kshitij-n24@users.noreply.github.com>
2023-12-01 22:43:51 +0530

    Neo middleware

--- server/src/middlewares/neo4jDatabase.js
+++ server/src/middlewares/neo4jDatabase.js
@@ -1,9 +1,11 @@
 import neo4j from 'neo4j-driver';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 
 const n4jDriver = neo4j.driver(
     'neo4j://localhost',
-    neo4j.auth.basic('neo4j', 'password')
+    neo4j.auth.basic(process.env.NEO_USER, 'password')
 );
 
 export default n4jDriver;
