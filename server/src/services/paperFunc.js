aa8b2d86195147161d14da14535acdf178784407
MMYaseen7 <yaseenmohommad7@gmail.com>
2023-12-02 03:24:50 +0530

    partial implementation of recPaper

--- server/src/services/paperFunc.js
+++ server/src/services/paperFunc.js
@@ -1,4 +1,7 @@
 import PaperModel from "../models/PaperSchema.js"
+import HistModel from "../models/HistSchema.js"
+import CommentModel from "../models/CommentSchema.js"
+import { Yake } from "yake-wasm";
 
 const getPapers = async (searchTerm,filter)=>{
     const db = PaperModel;
@@ -14,7 +17,30 @@ const getPaperPath = async (paperTitle) => {
 };
 
 const recPaper = async(email) => {
-    const db = PaperModel; return result;
+    const db = PaperModel; 
+    const commdb = CommentModel;
+    const histdb = HistModel;
+    let hist = await histdb.find({
+        "email" : email 
+    }).sort({'timestamp' : '-1'}).limit(5);
+
+    let comments = [];
+
+    for(let i in hist){
+        let comm = await commdb.find({
+            "email" :  email,
+            "paper_id" : i.paper_id,
+        })
+        comments.push(comm.comments.toArray())
+    }
+
+    let keyword_values = [];
+
+    let result = await db.find({
+        "keywords" : {
+            "$in" : keyword_values
+        }
+    })
 };
 
 export { getPaperPath, getPapers , recPaper};
