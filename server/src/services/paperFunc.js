3f1fdd34872a3d9beef89b154d8734c7d7fe212a
MMYaseen7 <yaseenmohommad7@gmail.com>
2023-12-02 15:23:03 +0530

    implemented recPaper

--- server/src/services/paperFunc.js
+++ server/src/services/paperFunc.js
@@ -1,7 +1,8 @@
 import PaperModel from "../models/PaperSchema.js"
 import HistModel from "../models/HistSchema.js"
 import CommentModel from "../models/CommentSchema.js"
-import { Yake } from "yake-wasm";
+import * as yake from './pkg/yake_wasm.js'
+
 
 const getPapers = async (searchTerm,filter)=>{
     const db = PaperModel;
@@ -25,22 +26,30 @@ const recPaper = async(email) => {
     }).sort({'timestamp' : '-1'}).limit(5);
 
     let comments = [];
-
     for(let i in hist){
         let comm = await commdb.find({
             "email" :  email,
             "paper_id" : i.paper_id,
         })
-        comments.push(comm.comments.toArray())
+        for(let i in comm.comments){
+            comments.push(i);
+        }
     }
+    // create yake new instance
+    const instance = new yake.Yake(); 
+    const in_string = comments.join(" ");
+    const raw_keys  = instance.get_n_best(in_string);
+    let keyword_values = []; // extract keywords from string
+    for(let obj_word in raw_keys){
+        keyword_values.push(obj_word["raw"]);
 
-    let keyword_values = [];
-
+    }
     let result = await db.find({
         "keywords" : {
             "$in" : keyword_values
         }
     })
+    return result;
 };
 
 export { getPaperPath, getPapers , recPaper};
