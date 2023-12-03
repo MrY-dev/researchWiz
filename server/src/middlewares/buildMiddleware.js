b4d68bca07704073286f7bb2f3ef59b15cf730ce
kshitij-n24 <141441196+kshitij-n24@users.noreply.github.com>
2023-12-03 21:54:39 +0530

    Do minor changes

--- server/src/middlewares/buildMiddleware.js
+++ server/src/middlewares/buildMiddleware.js
@@ -7,16 +7,16 @@ const buildMiddleware = (app) => {
   const __filename = fileURLToPath(import.meta.url);
   // eslint-disable-next-line no-underscore-dangle
   const __dirname = path.dirname(__filename);
-  const root = path.join(__dirname, '../../../client/build');
+  const root = path.join(__dirname, '../../client/build');
   app.use(express.static(root));
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
   });
 
   app.use((req, res) => {
     if (!req.path.includes('.')) {
-      res.sendFile(path.join(__dirname, '../../../client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
     }
   });
 
